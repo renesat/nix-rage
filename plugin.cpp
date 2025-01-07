19b1c71a1d939c4a0a0ff68f3a9b791e7d241505
renesat <self@renesat.me>
2025-01-07 01:57:55 +0100

    feat!: caching of decrypted files

--- plugin.cpp
+++ plugin.cpp
@@ -8,7 +8,7 @@ using namespace nix;
 
 extern "C" {
 char *nix_rage_decrypt(const char **identities, size_t size,
-                       const char *filename);
+                       const char *filename, bool cache, const char *cache_dir);
 char *nix_rage_decrypt_error();
 }
 
@@ -17,6 +17,9 @@ char *decrypt_content(EvalState &state, const PosIdx pos, Value **args) {
       *args[0], pos,
       "while evaluating the first argument passed to 'builtins.importAge'");
   state.forceValue(*args[1], pos);
+  state.forceAttrs(
+      *args[2], pos,
+      "while evaluating the first argument passed to 'builtins.importAge'");
 
   if (args[1]->type() != nPath) {
     state
@@ -42,8 +45,25 @@ char *decrypt_content(EvalState &state, const PosIdx pos, Value **args) {
     identities.push_back(const_cast<const char *>(path));
   }
 
-  auto content =
-      nix_rage_decrypt(identities.data(), identities.size(), filename);
+  auto cache_value = args[2]->attrs()->get(state.symbols.create("cache"));
+  bool cache = true;
+  if (cache_value) {
+    cache = cache_value->value->boolean();
+  }
+  auto cache_dir_value_ref =
+      args[2]->attrs()->get(state.symbols.create("cache_dir"));
+  const char *cache_dir = NULL;
+  if (cache_dir_value_ref) {
+    Value *cache_dir_value = cache_dir_value_ref->value;
+    if (cache_dir_value->type() != nPath) {
+      cache_dir =
+          const_cast<const char *>(cache_dir_value->path().path.c_str());
+    } else if (cache_dir_value->type() != nString) {
+      cache_dir = const_cast<const char *>(cache_dir_value->c_str());
+    }
+  }
+  auto content = nix_rage_decrypt(identities.data(), identities.size(),
+                                  filename, cache, cache_dir);
   if (!content) {
     auto err = nix_rage_decrypt_error();
     if (!err) {
@@ -93,16 +113,16 @@ void prim_readAgeFile(EvalState &state, const PosIdx pos, Value **args,
 static std::vector<RegisterPrimOp> primops = std::vector{
     nix::RegisterPrimOp((nix::PrimOp){
         .name = "importAge",
-        .args = {"identities", "path"},
-        .arity = 2,
+        .args = {"identities", "path", "configs"},
+        .arity = 3,
         .doc = "Import encypted .nix file",
         .fun = prim_importAge,
         .experimentalFeature = {},
     }),
     nix::RegisterPrimOp((nix::PrimOp){
         .name = "readAgeFile",
-        .args = {"identities", "path"},
-        .arity = 2,
+        .args = {"identities", "path", "configs"},
+        .arity = 3,
         .doc = "Read encrypted file",
         .fun = prim_readAgeFile,
         .experimentalFeature = {},
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 use age::{Decryptor, Identity, IdentityFile};
 use anyhow::Result;
+use sha256::digest;
 use std::ffi::{CStr, CString};
 use std::fs;
-use std::io::Read;
-use std::os::raw::c_char;
-use std::path::Path;
+use std::io::{Read, Write};
+use std::ops::Not;
+use std::os::{
+    raw::c_char,
+    unix::fs::{OpenOptionsExt, PermissionsExt},
+};
+use std::path::{Path, PathBuf};
 use std::ptr;
+use users::get_current_uid;
 
 fn decrypt_file<'a>(
     identities: impl Iterator<Item = &'a dyn Identity>,
@@ -19,7 +25,96 @@ fn decrypt_file<'a>(
     Ok(content)
 }
 
-fn _nix_rage_decrypt(identities: Vec<&str>, filename: &str) -> Result<String> {
+#[derive(Debug)]
+struct DecryptCache {
+    cache_dir: PathBuf,
+}
+
+impl Default for DecryptCache {
+    fn default() -> Self {
+        let mut cache_dir = tempfile::env::temp_dir();
+        cache_dir.push("nix-rage-cache");
+        Self { cache_dir }
+    }
+}
+
+impl DecryptCache {
+    fn new<P: AsRef<Path>>(cache_dir: P) -> Self {
+        Self {
+            cache_dir: cache_dir.as_ref().to_path_buf(),
+        }
+    }
+
+    fn user_cache_path(&self, uid: u32) -> std::path::PathBuf {
+        let mut cache_path = self.cache_dir.clone();
+        cache_path.push(uid.to_string());
+        cache_path
+    }
+
+    fn make_user_cache_path(&self, uid: u32) -> Result<()> {
+        let base_cache_path = &self.cache_dir;
+        if !fs::exists(base_cache_path)? {
+            fs::create_dir_all(base_cache_path)?;
+            let mut perms = fs::metadata(base_cache_path)?.permissions();
+            perms.set_mode(0o777);
+            fs::set_permissions(base_cache_path, perms)?;
+        }
+        let user_cache_path = self.user_cache_path(uid);
+        if !fs::exists(&user_cache_path)? {
+            fs::create_dir_all(&user_cache_path)?;
+            let mut perms = fs::metadata(&user_cache_path)?.permissions();
+            perms.set_mode(0o700);
+            fs::set_permissions(&user_cache_path, perms)?;
+        }
+        Ok(())
+    }
+
+    fn gen_cache_path<P: AsRef<Path>>(&self, path: P, uid: u32) -> std::path::PathBuf {
+        let cache_filename = path
+            .as_ref()
+            .to_str()
+            .map(digest)
+            .and_then(|f| {
+                path.as_ref()
+                    .file_name()
+                    .and_then(|n| n.to_str())
+                    .map(|n| format!("{f}-{n}"))
+            })
+            .expect("Incorrect file path!");
+        let mut cache_path = self.user_cache_path(uid);
+        cache_path.push(cache_filename);
+        cache_path
+    }
+
+    fn cache<P: AsRef<Path>>(&self, path: P, content: &str) -> Result<()> {
+        let uid = get_current_uid();
+        let cache_path = self.gen_cache_path(path, uid);
+        self.make_user_cache_path(uid)?;
+        let mut output = fs::OpenOptions::new()
+            .truncate(true)
+            .create(true)
+            .write(true)
+            .mode(0o700)
+            .open(cache_path)?;
+        write!(output, "{}", content)?;
+        Ok(())
+    }
+
+    fn load<P: AsRef<Path>>(&self, path: P) -> Result<Option<String>> {
+        let uid = get_current_uid();
+        let cache_path = self.gen_cache_path(path, uid);
+        if fs::exists(&cache_path)? {
+            return Ok(Some(fs::read_to_string(&cache_path)?));
+        }
+        Ok(None)
+    }
+}
+
+fn _nix_rage_decrypt(
+    identities: Vec<&str>,
+    filename: &str,
+    cache: Option<DecryptCache>,
+) -> Result<String> {
     let identities = identities
         .into_iter()
         .map(&str::to_string)
@@ -28,7 +123,16 @@ fn _nix_rage_decrypt(identities: Vec<&str>, filename: &str) -> Result<String> {
         .flatten()
         .collect::<Vec<_>>();
     let filename = Path::new(filename);
-    decrypt_file(identities.iter().map(|b| &**b), filename)
+    if let Some(cache) = &cache {
+        if let Some(content) = cache.load(filename)? {
+            return Ok(content);
+        }
+    }
+    let content = decrypt_file(identities.iter().map(|b| &**b), filename)?;
+    if let Some(cache) = &cache {
+        cache.cache(filename, &content)?;
+    }
+    Ok(content)
 }
 
 static mut DECRYPT_ERROR: Option<String> = None;
@@ -57,19 +161,34 @@ pub unsafe extern "C" fn nix_rage_decrypt(
     identities: *const *const c_char,
     size: usize,
     filename: *const c_char,
+    cache: bool,
+    cache_dir: *const c_char,
 ) -> *const c_char {
-    let fname = unsafe { CStr::from_ptr(filename) }.to_str();
+    let fname = unsafe { CStr::from_ptr(filename) }.to_str().ok();
+    let cache_dir = cache_dir
+        .is_null()
+        .not()
+        .then(|| unsafe { CStr::from_ptr(cache_dir) })
+        .map(CStr::to_str)
+        .and_then(Result::ok);
     let identities = unsafe { std::slice::from_raw_parts(identities, size) }
         .iter()
         .map(|ptr| unsafe { CStr::from_ptr(*ptr) }.to_str())
-        .collect::<Result<Vec<_>, _>>();
+        .collect::<Result<Vec<_>, _>>()
+        .ok();
     identities
-        .ok()
-        .and_then(|i| {
-            fname
+        .and_then(|i| fname.map(|f| (i, f)))
+        .map(|(i, f)| {
+            (
+                i,
+                f,
+                cache.then(|| cache_dir.map(DecryptCache::new).unwrap_or_default()),
+            )
+        })
+        .and_then(|(i, f, c)| {
+            _nix_rage_decrypt(i, f, c)
+                .map_err(|err| set_error(err.to_string()))
                 .ok()
-                .map(|f| _nix_rage_decrypt(i, f))
-                .and_then(|r| r.map_err(|err| set_error(err.to_string())).ok())
         })
         .and_then(|content| CString::new(content).ok())
         .map(|content| content.into_raw() as *const c_char)
