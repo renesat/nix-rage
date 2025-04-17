ca1ac2c7c84f8966a8ebdd3d26bfdf8ee7d9119e
S0mbr3 <benjamin_denis@outlook.com>
2025-04-17 16:51:02 +0200

    feat: support multiple nix versions
    Add supports for nix>=2.24
    
    Close #11

--- build.rs
+++ build.rs
@@ -23,6 +23,48 @@ impl AddPkg for cc::Build {
     }
 }
 
+struct NixVersion {
+    pub major: u64,
+    pub minor: u64,
+    pub patch: Option<u64>,
+    pub pre: Option<String>,
+}
+
+impl NixVersion {
+    fn from_str(version: &str) -> anyhow::Result<Self> {
+        let re = regex::Regex::new(r"(\d+).(\d+)(.(\d+)|)(pre(.+)|)$")?;
+        let caps = re
+            .captures(version)
+            .ok_or(anyhow::anyhow!("Fail to parse nix version!"))?;
+        let major = caps.get(1).unwrap().as_str().parse().unwrap();
+        let minor = caps.get(2).unwrap().as_str().parse().unwrap();
+        let patch = caps.get(4).map(|x| x.as_str().parse().unwrap());
+        let pre = caps.get(6).map(|x| x.as_str().to_string());
+        Ok(Self {
+            major,
+            minor,
+            patch,
+            pre,
+        })
+    }
+}
+
+impl std::fmt::Display for NixVersion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}.{}{}{}",
+            self.major,
+            self.minor,
+            self.patch.map(|x| format!(".{x}")).unwrap_or_default(),
+            self.pre
+                .as_ref()
+                .map(|x| format!("pre{x}"))
+                .unwrap_or_default()
+        )
+    }
+}
+
 fn main() {
     let nix_expr = pkg_config::Config::new()
         .atleast_version("2.24")
@@ -37,7 +79,10 @@ fn main() {
         .probe("nix-store")
         .unwrap();
 
-    let nix_ver = nix_expr.version.clone();
+    let nix_ver = NixVersion::from_str(&nix_expr.version).unwrap();
+    let nix_major_ver = nix_ver.major.to_string();
+    let nix_minor_ver = nix_ver.minor.to_string();
+    let nix_patch_ver = nix_ver.patch.unwrap_or_default().to_string();
 
     println!("cargo::rerun-if-changed=plugin.cpp");
     cc::Build::new()
@@ -49,7 +94,10 @@ fn main() {
         .add_pkg_config(nix_expr)
         .add_pkg_config(nix_store)
         .add_pkg_config(nix_main)
-        .define("BUILD_NIX_VERSION", Some(nix_ver.as_str()))
+        .define("NIX_MAJOR_VERSION", Some(nix_major_ver).as_deref())
+        .define("NIX_MINOR_VERSION", Some(nix_minor_ver).as_deref())
+        .define("NIX_PATCH_VERSION", Some(nix_patch_ver).as_deref())
+        .define("NIX_VERSION", Some(nix_ver.to_string()).as_deref())
         .cargo_metadata(true)
         .link_lib_modifier("+whole-archive")
         .compile("plugin");
